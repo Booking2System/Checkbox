fbe64ee11bc348aafa82c99ba2dc8f0773778163
Teerasak <maxz_drum@icloud.com>
2016-10-14 23:46:45 +0700

    Frist Project

---
+++ app/src/main/java/com/example/maxz/checkbox/Mydialog.java
@@ -0,0 +1,27 @@
+package com.example.maxz.checkbox;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+
+/**
+ * Created by maxz on 10/14/2016 AD.
+ */
+//สร้างอะเลิท
+public class Mydialog {
+
+    public void MyDialogDetail(Context context, String messeng, String title) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setMessage(messeng);
+        builder.setTitle(title);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+}
